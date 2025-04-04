6cee93a5cc5eceae6ed024b1e53f2bf32e9e85a4
Ankit <ankitsharma.as420@gmail.com>
2025-04-04 14:17:10 +0530

    added dependency for mail sender

--- src/main/java/com/example/Relationship20/Service/ServiceImpl/UserService.java
+++ src/main/java/com/example/Relationship20/Service/ServiceImpl/UserService.java
@@ -9,16 +9,11 @@ import com.example.Relationship20.Repository.UserRepository;
 import com.example.Relationship20.Service.ServiceInterface.UserInterface;
 import com.example.Relationship20.Service.Utility.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
